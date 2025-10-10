79c9b196f203f4ec3f51349e873b15f5df50944f
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-10-10 19:40:58 -0400

    Update realtime_texture_paint.py

--- realtime_texture_paint.py
+++ realtime_texture_paint.py
@@ -8,93 +8,110 @@ import trimesh
 import smplpact
 
 
-def smpl_unpack(person_list, device):
-    global_orient = torch.tensor([person['smpl_params']['global_orient'] for person in person_list], dtype=torch.float32, device=device)
-    body_pose = torch.tensor([person['smpl_params']['body_pose'] for person in person_list], dtype=torch.float32, device=device)
-    betas = torch.tensor([person['smpl_params']['betas'] for person in person_list], dtype=torch.float32, device=device)
-    smpl_params = { 'global_orient' : global_orient, 'body_pose' : body_pose, 'betas' : betas }
-    camera_translation = torch.tensor([person['camera_translation'] for person in person_list], dtype=torch.float32, device=device)
-    return smpl_params, camera_translation
-
-
 class demo:
     def run(self):
-        # Create offscreen renderer
-        viewport_width = 1280
-        viewport_height = 720
-        fov_vertical = np.pi / 3
-        fxy = smplpact.geometry_fov_to_f(fov_vertical, viewport_height)
+        # Settings
+        self._device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-        cfg_offscreen = smplpact.renderer_create_settings_offscreen(viewport_width, viewport_height)
+        self._smpl_test_message_path = './test_msg.txt'
+        self._smpl_model_path = './data/smpl/SMPL_NEUTRAL.pkl'
+        self._smpl_uv_path = './data/smpl_uv.obj'
+        self._smpl_texture_path = './data/textures/f_01_alb.002_1k.png'
+        self._smpl_texture_load_alpha = False
+        
+        self._viewport_width = 1280
+        self._viewport_height = 720
+
+        self._camera_fov_vertical = np.pi / 3
+        self._camera_yaw_increment = 10
+        self._camera_pitch_increment = 10
+        self._camera_distance_increment = 0.1
+        self._camera_use_plane = True
+        self._camera_focus_factor = 1.25
+
+        self._text_font_name = 'arial.ttf'
+        self._text_font_size = 512
+        self._text_font_color = (255, 0, 0, 255)
+        self._text_canvas_color = (255, 255, 255, 255)
+        self._text_stroke_width = 1
+        self._text_content = ['Probe', 'Here']
+        self._text_line_spacing = 20
+        self._text_pad_horizontal_ratio = 0.05
+        self._text_pad_vertical_ratio = 0.1
+        
+        self._cursor_radius = 0.015
+        self._cursor_height = 0.04
+        self._cursor_offset_increment = 0.02
+        self._cursor_angle_increment = np.radians(10)
+
+        self._brush_size = 0.01
+        self._brush_color_center = np.array([255, 0, 0, 255], dtype=np.uint8)
+        self._brush_color_edge = np.array([255, 255, 0, 255], dtype=np.uint8)
+        self._brush_hardness = 0.33
+        self._decal_size = 10000 * 2
+        self._decal_angle = 0
+        self._joint_projection_radius = 3
+        self._joint_projection_color = [255, 0, 255]
+
+        self._fps_period = 2.0
+        # End Settings
+
+        print(f'Using device: {self._device}')
+
+        # Create offscreen renderer
+        fxy = smplpact.geometry_fov_to_f(self._camera_fov_vertical, self._viewport_height)
+
+        cfg_offscreen = smplpact.renderer_create_settings_offscreen(self._viewport_width, self._viewport_height)
         cfg_scene = smplpact.renderer_create_settings_scene()
-        cfg_camera = smplpact.renderer_create_settings_camera(fxy, fxy, viewport_width // 2, viewport_height // 2)
+        cfg_camera = smplpact.renderer_create_settings_camera(fxy, fxy, self._viewport_width // 2, self._viewport_height // 2)
         cfg_camera_transform = smplpact.renderer_create_settings_camera_transform()
         cfg_lamp = smplpact.renderer_create_settings_lamp()
         
         self._offscreen_renderer = smplpact.renderer(cfg_offscreen, cfg_scene, cfg_camera, cfg_camera_transform, cfg_lamp)
 
-        # Load textures and UV map
-        self._texture_array = smplpact.texture_load_image('./data/textures/f_01_alb.002_1k.png')
-        self._texture_array_2 = smplpact.texture_load_image('./data/textures/smpl_uv_20200910.png', False)
-        self._test_stamp = smplpact.texture_load_image('./data/textures/stamp_test.jpg')
-        self._offscreen_renderer.smpl_load_uv('./data/smpl_uv.obj', self._texture_array.shape)
-
         # Create sample text texture
-        font = smplpact.texture_load_font('arial.ttf', 512)
-        self._test_text = smplpact.texture_create_multiline_text(['Sample', 'Text'], font, (255, 0, 0, 255), (255, 255, 255, 255), 1, 20)
-        self._test_text = smplpact.texture_pad(self._test_text, 0.05, 0.1, (255, 255, 255, 255))
+        font = smplpact.texture_load_font(self._text_font_name, self._text_font_size)
+        self._test_text = smplpact.texture_create_multiline_text(self._text_content, font, self._text_font_color, self._text_canvas_color, self._text_stroke_width, self._text_line_spacing)       
+        self._test_text = smplpact.texture_pad(self._test_text, self._text_pad_horizontal_ratio, self._text_pad_vertical_ratio, self._text_canvas_color)
+
+        # Load SMPL texture
+        self._texture_array = smplpact.texture_load_image(self._smpl_texture_path, load_alpha=self._smpl_texture_load_alpha)
 
         # Load SMPL model
-        self._device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-        self._offscreen_renderer.smpl_load_model('data/smpl/SMPL_NEUTRAL.pkl', 10, self._device)
-        print(f'Using device: {self._device}')
+        self._offscreen_renderer.smpl_load_model(self._smpl_model_path, 10, self._device)
+        self._offscreen_renderer.smpl_load_uv(self._smpl_uv_path, self._texture_array.shape)
 
+        # Load test CameraHMR message
+        with open(self._smpl_test_message_path, 'r') as json_file:
+            test_camerahmr_message = json.load(json_file)
 
-        
-        
-        
-        
         # SMPL regions
         self._smpl_regions = ['body_center', 'thigh_left', 'thigh_right', 'lower_leg_left', 'lower_leg_right', 'foot_left', 'foot_right', 'head_center', 'upper_arm_left', 'upper_arm_right', 'lower_arm_left', 'lower_arm_right']
         self._smpl_region_index = 0
         self._smpl_region = self._smpl_regions[self._smpl_region_index]
-        self._focus_factor = 1.25
         
         # Create UI elements
-        self._cursor_radius = 0.015
-        self._cursor_height = 0.04
-        self._cursor_offset_increment = 0.02
-        self._cursor_angle_increment = np.radians(10)
         self._cursor_mesh = trimesh.creation.cone(radius=self._cursor_radius, height=self._cursor_height)
         self._cursor_pose = np.eye(4, dtype=np.float32)
         self._cursor_offset = 0
         self._cursor_angle = 0
 
-        # Camera translation mode
-        self._camera_use_plane = True
-
-        
-        
-
-        # Load test CameraHMR message
-        with open('./test_msg.txt', 'r') as json_file:
-            test_camerahmr_message = json.load(json_file)
-        
         # Run inference and painting
         start = time.perf_counter()
         count = 0
         while (self._loop(test_camerahmr_message)):
             count += 1
             end = time.perf_counter()
-            if (end-start > 2.0):
-                print(f'FPS: ', count / (end-start))
+            delta = end - start
+            if (delta > self._fps_period):
+                print(f'FPS: ', count / delta)
                 start = end
                 count = 0
 
     def _loop(self, camerahmr_message):
         # SMPL params to mesh
         person_list = camerahmr_message['persons']
-        smpl_params, camera_translation = smpl_unpack(person_list, self._device)
+        smpl_params, camera_translation = self.smpl_unpack(person_list)
         smpl_result = self._offscreen_renderer.smpl_get_mesh(smpl_params, camera_translation)
         smpl_vertices = smpl_result.vertices[0]
         smpl_joints = smpl_result.joints[0]
@@ -120,7 +137,7 @@ class demo:
             focus_points = smplpact.math_transform_points(smpl_frame.points, smpl_mesh_pose.T, inverse=False)
             focus_distance = self._offscreen_renderer.camera_solve_fov_z(focus_center, focus_points)
 
-            self._offscreen_renderer.camera_adjust_parameters(center=focus_center, distance=self._focus_factor * focus_distance, relative=False)
+            self._offscreen_renderer.camera_adjust_parameters(center=focus_center, distance=self._camera_focus_factor * focus_distance, relative=False)
             self._cursor_offset = 0
             self._cursor_angle = 0
             self._smpl_region = smpl_next_region
@@ -156,12 +173,12 @@ class demo:
         if (cursor_anchor.point is not None):
             # Fix loose degree of freedom about face normal, required to maintain consistent orientation
             decal_align_prior = self._offscreen_renderer.smpl_paint_decal_align_prior(smpl_mesh_id, cursor_anchor, smpl_frame.up, smpl_frame.front)
-            self._offscreen_renderer.smpl_paint_decal_solid(smpl_mesh_id, cursor_anchor, self._test_text, decal_align_prior, 0, 10000 * 2, double_cover_test=False, fill_test=0.25)
+            self._offscreen_renderer.smpl_paint_decal_solid(smpl_mesh_id, cursor_anchor, self._test_text, decal_align_prior, self._decal_angle, self._decal_size, double_cover_test=False, fill_test=0.25)
         # Paint circular gradient at camera mesh intersection/closest
         if (camera_anchor.point is not None):
-            self._offscreen_renderer.smpl_paint_brush_gradient(smpl_mesh_id, camera_anchor, 0.01, np.array([255, 0, 0, 255], dtype=np.uint8), np.array([255, 255, 0, 255], dtype=np.uint8), 0.33, fill_test=0.25)
+            self._offscreen_renderer.smpl_paint_brush_gradient(smpl_mesh_id, camera_anchor, self._brush_size, self._brush_color_center, self._brush_color_edge, self._brush_hardness, fill_test=0.25)
             # Solid color option
-            #self._offscreen_renderer.smpl_paint_brush_solid(smpl_mesh_id, camera_anchor, 0.02, np.array([0, 255, 0, 255], dtype=np.uint8), 0.25)
+            #self._offscreen_renderer.smpl_paint_brush_solid(smpl_mesh_id, camera_anchor, self._brush_size, self._brush_color_center, fill_test=0.25)
         
         # Finalize SMPL painting
         # Compute painted texture
@@ -184,7 +201,7 @@ class demo:
         for i in range(0, image_points.shape[0]):
             if (local_points[i, 2] > 0):
                 center = (int(image_points[i, 0]), int(image_points[i, 1]))
-                color = cv2.circle(color, center, 3, [255, 0, 255], -1)
+                color = cv2.circle(color, center, self._joint_projection_radius, self._joint_projection_color, -1)
 
         # Show rendered image
         cv2.imshow('offscreen test', cv2.cvtColor(color, cv2.COLOR_RGB2BGR))
@@ -205,34 +222,42 @@ class demo:
             self._cursor_angle -= self._cursor_angle_increment
 
         if (key == 68 or key == 100): # d
-            self._offscreen_renderer.camera_adjust_parameters(yaw=10, relative=True)
+            self._offscreen_renderer.camera_adjust_parameters(yaw=self._camera_yaw_increment, relative=True)
         if (key == 65 or key == 97): # a
-            self._offscreen_renderer.camera_adjust_parameters(yaw=-10, relative=True)
+            self._offscreen_renderer.camera_adjust_parameters(yaw=-self._camera_yaw_increment, relative=True)
         if (key == 87 or key == 119): # w
-            self._offscreen_renderer.camera_adjust_parameters(pitch=-10, relative=True)
+            self._offscreen_renderer.camera_adjust_parameters(pitch=-self._camera_pitch_increment, relative=True)
         if (key == 83 or key == 115): # s
-            self._offscreen_renderer.camera_adjust_parameters(pitch=10, relative=True)
+            self._offscreen_renderer.camera_adjust_parameters(pitch=self._camera_pitch_increment, relative=True)
         if (key == 82 or key == 114): #r
-            self._offscreen_renderer.camera_adjust_parameters(distance=-0.1, relative=True)
+            self._offscreen_renderer.camera_adjust_parameters(distance=-self._camera_distance_increment, relative=True)
         if (key == 70 or key == 102): #f
-            self._offscreen_renderer.camera_adjust_parameters(distance=0.1, relative=True)
+            self._offscreen_renderer.camera_adjust_parameters(distance=self._camera_distance_increment, relative=True)
         if (key == 78 or key == 110): #n
-            self._offscreen_renderer.camera_move_center([-0.1, 0, 0], plane=self._camera_use_plane)
+            self._offscreen_renderer.camera_move_center([-self._camera_distance_increment, 0, 0], plane=self._camera_use_plane)
         if (key == 77 or key == 109): #m
-            self._offscreen_renderer.camera_move_center([0.1, 0, 0], plane=self._camera_use_plane)
+            self._offscreen_renderer.camera_move_center([self._camera_distance_increment, 0, 0], plane=self._camera_use_plane)
         if (key == 85 or key == 117): #u
-            self._offscreen_renderer.camera_move_center([0, 0.1, 0], plane=self._camera_use_plane)
+            self._offscreen_renderer.camera_move_center([0, self._camera_distance_increment, 0], plane=self._camera_use_plane)
         if (key == 74 or key == 106): #j
-            self._offscreen_renderer.camera_move_center([0, -0.1, 0], plane=self._camera_use_plane)
+            self._offscreen_renderer.camera_move_center([0, -self._camera_distance_increment, 0], plane=self._camera_use_plane)
         if (key == 73 or key == 105): #i
-            self._offscreen_renderer.camera_move_center([0, 0, -0.1], plane=self._camera_use_plane)
+            self._offscreen_renderer.camera_move_center([0, 0, -self._camera_distance_increment], plane=self._camera_use_plane)
         if (key == 75 or key == 107): #k
-            self._offscreen_renderer.camera_move_center([0, 0, 0.1], plane=self._camera_use_plane)
+            self._offscreen_renderer.camera_move_center([0, 0, self._camera_distance_increment], plane=self._camera_use_plane)
 
         if (key == 27): # esc
             return False
         
         return True
+
+    def smpl_unpack(self, person_list):
+        global_orient = torch.tensor([person['smpl_params']['global_orient'] for person in person_list], dtype=torch.float32, device=self._device)
+        body_pose = torch.tensor([person['smpl_params']['body_pose'] for person in person_list], dtype=torch.float32, device=self._device)
+        betas = torch.tensor([person['smpl_params']['betas'] for person in person_list], dtype=torch.float32, device=self._device)
+        smpl_params = { 'global_orient' : global_orient, 'body_pose' : body_pose, 'betas' : betas }
+        camera_translation = torch.tensor([person['camera_translation'] for person in person_list], dtype=torch.float32, device=self._device)
+        return smpl_params, camera_translation
 
 
 def main():
